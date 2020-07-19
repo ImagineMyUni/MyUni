7a10a5afbc8f0ff11124a652d592fcb26b5f28be
asomeJay <45970468+asomeJay@users.noreply.github.com>
2020-07-19 10:57:27 +0900

    :art: Add middleware to Router
    권한에 따라 접근 제한을 시켰습니다. 예를 들어서 이미 로그인한 사람이 또 로그인 창에 들어가면 안되겠죠

--- server/router/apiRouter.js
+++ server/router/apiRouter.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import routes from './routes';
 import { postScoreConverter, getScoreConverter } from '../controller/apiController';
+import { onlyPrivate } from '../middleware/auth';
 
 const apiRouter = express.Router();
 
-apiRouter.post(routes.converter, postScoreConverter);
-apiRouter.get(routes.converter, getScoreConverter);
+apiRouter.post(routes.converter, onlyPrivate, postScoreConverter);
+apiRouter.get(routes.converter, onlyPrivate, getScoreConverter);
 
 export default apiRouter;
--- server/router/globalRouter.js
+++ server/router/globalRouter.js
@@ -12,34 +12,34 @@ const globalRouter = express.Router();
 
 globalRouter.get(routes.home, home);
 
-globalRouter.get(routes.login,getLogin);
+globalRouter.get(routes.login,onlyPublic, getLogin);
 // globalRouter.post(routes.login, postLogin);
 
-globalRouter.get(routes.logout, getLogout);
+globalRouter.get(routes.logout, onlyPrivate,getLogout);
 
-globalRouter.get(routes.join, isAuthenticated, getJoin);
+globalRouter.get(routes.join, onlyPublic, getJoin);
 // globalRouter.post(routes.join, postJoin);
 
-globalRouter.get(routes.google, googleLogin);
-globalRouter.get(routes.googleCallback,
+globalRouter.get(routes.google, onlyPublic, googleLogin);
+globalRouter.get(routes.googleCallback, onlyPublic,
     passport.authenticate('google', { failureRedirect: '/login' }),
     postGoogleLogin
 );
 
-globalRouter.get(routes.kakao, kakaoLogin);
-globalRouter.get(routes.kakaoCallback,
+globalRouter.get(routes.kakao, onlyPublic, kakaoLogin);
+globalRouter.get(routes.kakaoCallback, onlyPublic,
     passport.authenticate('kakao', { failureRedirect: '/login' }),
     postKakaoLogin
 );
 
-globalRouter.get(routes.naver, naverLogin);
-globalRouter.get(routes.naverCallback,
+globalRouter.get(routes.naver, onlyPublic,naverLogin);
+globalRouter.get(routes.naverCallback, onlyPublic,
     passport.authenticate('naver', { failureRedirect: '/login' }),
     postNaverLogin
 );
 
-globalRouter.get(routes.facebook, facebookLogin);
-globalRouter.get(routes.facebookCallback,
+globalRouter.get(routes.facebook, onlyPublic, facebookLogin);
+globalRouter.get(routes.facebookCallback, onlyPublic,
     passport.authenticate('facebook', { failureRedirect: '/login' }),
     postFacebookLogin
 );
